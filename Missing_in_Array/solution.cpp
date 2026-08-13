7b1477a3a3e001d493a8e6a84c474db5ca109b5a
Sharath M <sharathmnayak2005@gmail.com>
2026-08-13 18:20:49 +0530

    Delete Missing_in_Array directory

--- Missing_in_Array/solution.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-  public:
-    int missingNum(int n, vector<int>& arr) {
-        long long total = (long long)n * (n + 1) / 2;
-        long long sum = 0;
-        for(int x : arr) sum += x;
-        return total - sum;
-    }
-};
