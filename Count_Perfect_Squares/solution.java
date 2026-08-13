0feb16a03caa3d022f53edf51ca053eec292da73
Sharath M <sharathmnayak2005@gmail.com>
2026-08-13 19:05:16 +0530

    Solved: Count Perfect Squares

---
+++ Count_Perfect_Squares/solution.java
@@ -0,0 +1,1 @@
+{"code":"class Solution {\n    static int countSquares(int n) {\n        // code here\n        if (n <= 1) {\n            return 0;\n        }\n        return (int) Math.sqrt(n - 1);\n    }\n}","lang":"java","pid":704184}
